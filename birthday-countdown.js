4a4239b08dce91425d03308c333847524ecb5651
Jorge Cano <jacinaustin@gmail.com>
2017-03-28 17:43:54 -0500

    hw and bonuses

--- birthday-countdown.js
+++ birthday-countdown.js
@@ -27,3 +27,42 @@
 */
 
 // YOUR CODE HERE
+
+// Psuedo CODE
+// console.log("first name")
+// response var = firstName;
+// console.log("Enter your birthdate");
+// var birthday = birthdate || form(mm/dd/yyyy);
+// daysUntilDate = (birthday - date);
+// birthdayReminder("Your birthday is in " + daysUntilDate + "days");
+
+var currentDate = new Date();
+var oneDay = 24*60*60*1000;
+
+function main(){
+  var birthdayArray = [
+      {
+        name: "Jack",
+        dob: "10/31/2013"
+      },
+      {
+        name: "Jill",
+        dob: "4/01/1975"
+      }
+    ];
+  birthdayReminder(birthdayArray);
+}
+function birthdayReminder(birthdayArray){
+   for (i=0, x = birthdayArray.length; i<x;i++){
+    var name = birthdayArray[i].name;
+    var dob = birthdayArray[i].dob;
+    var newDateArray = dob.split('/');
+    var bday = new Date(currentDate.getFullYear(),newDateArray[0]-1,newDateArray[1]);
+   if (currentDate > bday){
+    bday.setFullYear(bday.getFullYear() + 1);
+    }
+  var diff = Math.floor((bday-currentDate)/oneDay);
+  console.log(name + "'s birthday is in "+ diff + ' days.');
+  }
+}
+main();
--- letter-count.js
+++ letter-count.js
@@ -26,3 +26,67 @@
 */
 
 // YOUR CODE HERE
+
+// var y = 1989;
+// var m = 3;
+// var d = 23;
+
+// function getDifference(y,m,d) {
+//   var date = y + '.' + m + '.' + d;
+//   console.log(date)
+//   var difference = ((new Date().getTime() - new Date(date).getTime() / 1000) / 60 ) / 60) / 24) / 365);
+//   var seconds = difference / 1000;
+//   var minutes
+//   var hours
+//   var days
+//    var years
+//   return Math.floor(difference) + 'years!';
+// getDifference(y,m,d)
+// }
+
+/*
+ -  Create a function `letterCount` that accepts a string, and finds the number of times each letter
+ -  occurs in the string. For example, given the word "apple", letterCount("apple") should count all
+ -  occurrences of the letters "a", "p", "l" and "e" and then return the following output:
+ -  ```javascript
+ -  {
+ -    "a": 1,
+ -    "p": 2,
+ -    "l": 1,
+ -    "e": 1
+ -  }
+ -  ```
+ -  Bonuses
+ -  - Make sure that lower case letters and upper case letters count for the same character.
+ -  - Ignore spaces, special characters, and punctuation.
+ -  - Instead of just counting letters, calculate their percent-based frequency.
+ -    See: http://www.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+ -    ```javascript
+ -    {
+ -      "a": 0.2, // percent
+ -      "p": 0.4,
+ -      "l": 0.2,
+ -      "e": 0.2
+ -    }
+ -    ```
+ -*/
+
+ var string = 'apple';
+
+ letterCount(string);
+
+ function letterCount(string){
+   var lowerCase = string.toLowerCase();
+   var noSpaces = lowerCase.replace(/\s/g, '');
+   var noPuncuation = noSpaces.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+   var stringArray = noSpaces.split('');
+   var letters = {};
+   for (var i = 0, x = stringArray.length; i<x;i++){
+     if (!letters[stringArray[i]]){
+       letters[stringArray[i]] = 1/x;
+     } else{
+       letters[stringArray[i]]+=1/x;
+    }
+  }
+  console.log(letters);
+}
