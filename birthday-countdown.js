c622e7432bf41e54390c30b553c46c74e7b20026
b b <bboutette@gmail.com>
2017-05-09 19:20:33 -0500

    dummy data working, but object array not working on dates past.

--- birthday-countdown.js
+++ birthday-countdown.js
@@ -1,29 +1,39 @@
-/*
-  Create a function `daysUntilDate` that accepts a string (with the format `"mm/dd/yyyy"`) and
-  returns the number of days (integer) between today and that date. Please use the built in
-  Javascript `Date` type in your solution.
-  See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-  Next, create a function `birthdayReminder` that accepts an array of objects, containing a person's
-  date of birth (dob), and returns an array of reminder messages (strings).
-  ```javascript
-  birthdayReminder([
-    {
-      name: "Jack",
-      dob: "10/31/2013"
-    },
-    {
-      name: "Jill",
-      dob: "4/01/1975"
-    }
-  ]);
-  //=> [
-  //      "Jack's birthday is in 75 days",
-  //      "Jill's birthday is in 305 days"
-  //    ]
-  ```
-  Bonuses
-  - Will your function still work tomorrow when the date is different? Is it future proofed?
-  - To make your output more relevant, can you sort by days remaining (ascending)?
-*/
+// Finds days until next birthday.  Works w/ test data
+function daysUntilDate(date) {
+   var month = date.substr(0,2)-1;
+   var day = date.substr(3,2);
+   var year = new Date().getFullYear();
+   var today = new Date();
+   var checkDate = new Date(year, month, day);
 
-// YOUR CODE HERE
+  d = Math.floor((checkDate-today)/86400000);
+
+  if( d < 0) {
+    return (d + 365)
+  } else {
+    return d
+  }
+}
+
+// Testing daysUntilDate function
+daysUntilDate('04/19/81');
+
+// Birthday objects to run through functions.
+var birthdays= ([
+{
+name: "Jack",
+dob: "10/31/2013"
+},
+{
+name: "Jill",
+dob: "4/01/1975"
+}
+]);
+// Runs through function objects and drills down to birthdays, then evaluating how many days until w our daysUntilDate function.
+function birthdayReminder(b) {
+    for (var i = 0, x= b.length; i<x; i++){
+      console.log(b[i].name + 's birthday is in ' + daysUntilDate(b[i].dob) + ' days');
+   }
+  }
+
+  birthdayReminder(birthdays)
